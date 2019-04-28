272979483648aacd904cfba5c21f67bed9e464cf
eolvwa <eolvwa@gmail.com>
2019-04-27 20:58:42 -0400

    Update the arbitraryOverlay function to be a bit more idiomatic to Haskell

--- hands-on.hs
+++ hands-on.hs
@@ -33,23 +33,22 @@ overlayCheckers (CharCanvas { width = w, height = h }) =
   in map (map $ \(x) -> if x == 1 then ' ' else '#') onesZeroes -- Convert onesZeroes to ["# #"," # ","# #"," # "]
 
 
-
--- list of row vectors
+-- Given canvas dimensions, return a nested array of coordinates:    
+--   [ Rows ] == [ [ Row1Col1, Row1Col2, ... ], ... ] = [ [ (1,1), (2,1), ... ], [ [1,2), (2,2), ... ], ... ]
+-- Note that the tuples are arranged as (Column #, Row #).
+--
 coordinatizer :: CharCanvas -> [[(Int, Int)]]
-coordinatizer (CharCanvas { width = w, height = h }) =
-  let m = map (zip [1..w] . replicate w) [1..h]
-  in m
+coordinatizer (CharCanvas { width = w, height = h }) = map (zip [1..w] . replicate w) [1..h]
 
 
+-- Return an array of strings, one for each row,
+-- given canvas dimensions and a function that translates a (column, row) tuple into a Char.
+--
+-- Usage: overlayArbitrary (CharCanvas 10 5) (\(col, row) -> if col `mod` 2 == 0 then 'X' else '*')
 overlayArbitrary :: CharCanvas -> ( (Int,Int) -> Char ) -> [String]
-overlayArbitrary canvas f  =
-  let coords = coordinatizer canvas
-  -- row:[ columns:[ String, ... ] ]  -> row:[ column:[ string1, string2, ... ] ]  <=== this one
-  --                                  -> row strings: [ string1, string 2, ]  <== no
-  --                                  -> string    <== no
-  in let mappedStrs = map rowOperator coords
-  in mappedStrs
+overlayArbitrary canvas f  = map rowOperator coords
   where
-    rowOperator :: [(Int, Int)] -> String
+    rowOperator :: [(Int, Int)] -> String  -- wrap f so we can use it in an row = array of tuples for the row
     rowOperator = map f
 
+    coords = coordinatizer canvas -- get the canvas as a coordinate stream
